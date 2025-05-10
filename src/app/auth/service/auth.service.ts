7d206c4e1d62b84c15a18874281665ccb7de797b
powidev <calderon4924@gmail.com>
2025-05-10 18:20:01 -0500

    CRUD funcional de ADMIN y SUPERVISOR

--- src/app/auth/service/auth.service.ts
+++ src/app/auth/service/auth.service.ts
@@ -2,29 +2,38 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Registrar } from '../../Interface/registrar';
+import { Router } from '@angular/router';  // Importar Router
 
 @Injectable({
-  providedIn: 'root'// Esto hace que Angular cree una sola instancia (Singleton)
+  providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:8090/api/auth'
+  private apiUrl = 'http://localhost:8090/api/auth';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
+
+  getProfile(): Observable<any> {
+  return this.http.get<any>('http://localhost:8090/api/auth/profile');
+  }
 
   login(credentials: { username: string; password: string }): Observable<any> {
     return this.http.post<any>(`${this.apiUrl}/login`, credentials);
   }
-  
+
   register(request: Registrar): Observable<{ message: string }> {
     return this.http.post<{ message: string }>(`${this.apiUrl}/register`, request);
   }
-  
+
   logout(): void {
     localStorage.removeItem('token');
+    console.log('Token eliminado');
+    this.router.navigate(['/login']);
   }
-  isAutheticated(): boolean{
+
+  isAuthenticated(): boolean {
     return !!localStorage.getItem('token');
   }
+
   getToken(): string | null {
     return localStorage.getItem('token');
   }
